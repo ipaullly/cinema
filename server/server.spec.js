e6431ef75bcd8e0a098313903639d103e2b6175a
ipaullly <ipaully22@gmail.com>
2019-11-29 07:39:57 +0300

    Add tests, controllers

---
+++ server/server.spec.js
@@ -0,0 +1,15 @@
+/* eslint-env mocha */
+const server = require('./server')
+
+describe('Server', () => {
+    it('should require a port to start', () => {
+        return server.start({
+            repo: {}
+        }).should.be.rejectedWith(/port/)
+    })
+    it('should require a repository to start', () => {
+        return server.start({
+            repo: {}
+        }).should.be.rejectedWith(/repository/)
+    })
+})
